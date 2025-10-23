91d671a657f0e237bf4e6728dcef6ab40c6b39e5
Damir.? <vip.ushkempir@mail.ru>
2025-10-23 11:16:49 +0500

    Structure

---
+++ src/model/Graph.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+    public int id;
+    public List<String> nodes = new ArrayList<>();
+    public List<Edge> edges = new ArrayList<>();
+
+    public Graph(int id) {
+        this.id = id;
+    }
+}
